31c7e8bd1485815a685ff5359e3f17bc4e568f5a
NaderGesmi <nedergesmi35@gmail.com>
2022-04-05 05:48:39 +0100

    backend user jwt + product

---
+++ api/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
+const productRoute = require("./routes/product")
+
+dotenv.config();
+/* Connect To DB */
+mongoose
+    .connect(
+        process.env.MONGO_URL
+    )
+    .then(() =>
+        console.log("DB Connection Successfull !")
+    ).catch((err) =>
+        console.log(err));
+app.use(express.json());
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/product", productRoute);
+
+app.listen(process.env.PORT, () => {
+    console.log("backend server is Running! at " + process.env.PORT);
+})
