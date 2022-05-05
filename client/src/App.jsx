638e8617b614b7ef7d10254b163c6dda7bec84f5
GuesmiNader <nadergesmi40@gmail.com>
2022-05-05 05:59:25 +0100

    fix router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,45 @@
-
 import Home from "./pages/Home";
-
-
+import Cart from "./pages/Cart";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
 const App = () => {
-  return <Home/>;
+  const user = true;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products/:category">
+          <ProductList />
+        </Route>
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        {/* <Route path="/success">
+        <Success />
+      </Route> */}
+        <Route path="/login">
+          {user ? <Redirect to="/" /> : <Login />}
+          <Login />
+        </Route>
+        <Route path="/register">
+          {user ? <Redirect to="/" /> : <Register />}
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
